ce831aabbc5af6325b2992e9fdbe07bf2f6d1a64
Trend20 <enockomondi305@gmail.com>
2024-09-02 12:51:05 +0300

    creating the native server and adding test routes and handlers

--- cmd/standardlib/main.go
+++ cmd/standardlib/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello standard library")
+
+	//	initialize a server multiplexer
+	mux := http.NewServeMux()
+
+	//	register the routes and handlers
+	mux.Handle("/", &homeHandler{})
+	mux.Handle("/recipes", &recipeHandler{})
+
+	//	server listen on port 3000
+	err := http.ListenAndServe(":3000", mux)
+	if err != nil {
+		return
+	}
+}
+
+// homepage initializations
+type homeHandler struct{}
+
+// define the homeHandler method
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the homepage"))
+}
+
+// recipes initializations
+type recipeHandler struct{}
+
+// define the recipeHandler method
+func (h *recipeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("This is the recipe handler method"))
 }
