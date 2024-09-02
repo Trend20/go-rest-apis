611c01db5078e55aa963ede3c31921902970ac22
Trend20 <enockomondi305@gmail.com>
2024-09-02 15:24:23 +0300

    adding the create recipe handler

--- cmd/standardlib/main.go
+++ cmd/standardlib/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/Trend20/go-rest-apis/pkg/recipes"
+	"github.com/gosimple/slug"
 	"net/http"
 	"regexp"
 )
@@ -10,10 +13,14 @@ func main() {
 	//	initialize a server multiplexer
 	mux := http.NewServeMux()
 
+	// Create the Store and Recipe Handler
+	store := recipes.NewMemStore()
+	recipesHandler := NewRecipesHandler(store)
+
 	//	register the routes and handlers
 	mux.Handle("/", &homeHandler{})
-	mux.Handle("/recipes", &RecipesHandler{})
-	mux.Handle("/recipes/", &RecipesHandler{})
+	mux.Handle("/recipes", recipesHandler)
+	mux.Handle("/recipes/", recipesHandler)
 
 	//	server listen on port 3000
 	err := http.ListenAndServe(":3000", mux)
@@ -22,14 +29,77 @@ func main() {
 	}
 }
 
+// homeHandler initialization
+type homeHandler struct{}
+
+// define the homeHandler method
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome to the homepage"))
+}
+
+// ERROR HANDLING
+func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusInternalServerError)
+	w.Write([]byte("500 Internal Server Error"))
+}
+
+func NotFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("404 Not Found"))
+}
+
+// recipe store interface
+type recipeStore interface {
+	Add(name string, recipe recipes.Recipe) error
+	Get(name string) (recipes.Recipe, error)
+	Update(name string, recipe recipes.Recipe) error
+	List() (map[string]recipes.Recipe, error)
+	Remove(name string) error
+}
+
 // RecipesHandler initialization
-type RecipesHandler struct{}
+type RecipesHandler struct {
+	store recipeStore
+}
+
+func NewRecipesHandler(s recipeStore) *RecipesHandler {
+	return &RecipesHandler{
+		store: s,
+	}
+}
 
 // create function handlers for different routes
-func (h *RecipesHandler) CreateRecipe(w http.ResponseWriter, r *http.Request) {}
-func (h *RecipesHandler) ListRecipes(w http.ResponseWriter, r *http.Request)  {}
-func (h *RecipesHandler) GetRecipe(w http.ResponseWriter, r *http.Request)    {}
+
+// CREATE RECIPE
+func (h *RecipesHandler) CreateRecipe(w http.ResponseWriter, r *http.Request) {
+	// Recipe object that will be populated from JSON payload
+	var recipe recipes.Recipe
+	if err := json.NewDecoder(r.Body).Decode(&recipe); err != nil {
+		InternalServerErrorHandler(w, r)
+		return
+	}
+	// Convert the name of the recipe into URL friendly string
+	resourceID := slug.Make(recipe.Name)
+
+	// Call the store to add the recipe
+	if err := h.store.Add(resourceID, recipe); err != nil {
+		InternalServerErrorHandler(w, r)
+		return
+	}
+	// Set the status code to 200
+	w.WriteHeader(http.StatusOK)
+}
+
+// GET ALL RECIPES
+func (h *RecipesHandler) ListRecipes(w http.ResponseWriter, r *http.Request) {}
+
+// GET RECIPE BY ID
+func (h *RecipesHandler) GetRecipe(w http.ResponseWriter, r *http.Request) {}
+
+// UPDATE RECIPE
 func (h *RecipesHandler) UpdateRecipe(w http.ResponseWriter, r *http.Request) {}
+
+// DELETE RECIPE
 func (h *RecipesHandler) DeleteRecipe(w http.ResponseWriter, r *http.Request) {}
 
 // REGEX CHECKER
@@ -59,12 +129,4 @@ func (h *RecipesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	default:
 		return
 	}
-}
-
-// homeHandler initialization
-type homeHandler struct{}
-
-// define the homeHandler method
-func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Welcome to the homepage"))
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/Trend20/go-rest-apis
 
 go 1.22.6
+
+require (
+	github.com/gosimple/slug v1.14.0 // indirect
+	github.com/gosimple/unidecode v1.0.1 // indirect
+)
